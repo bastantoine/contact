7f11209658f6a5e808880dd4fa8e7c467c33b954
bastantoine <bastien.antoine69+github@gmail.com>
2021-03-18 15:49:03 +0100

    Load config in the Home component

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -7,8 +7,15 @@ import { join } from "../utils";
 type PropsType = {}
 type StateType = {
     isLoaded: boolean,
+    isConfigLoaded: boolean,
     error: null | JQuery.jqXHR,
     contacts: any[],
+    config: {
+        main_attributes: string[],
+        attributes: string[],
+        primary_key: string,
+        raw_config: any,
+    },
 }
 
 class Home extends Component<PropsType, StateType> {
@@ -17,12 +24,61 @@ class Home extends Component<PropsType, StateType> {
         super(props);
         this.state = {
             isLoaded: false,
+            isConfigLoaded: false,
             error: null,
-            contacts: []
+            contacts: [],
+            config: {
+                main_attributes: [],
+                attributes: [],
+                primary_key: '',
+                raw_config: {}
+            },
         }
     }
 
-    componentDidMount() {
+    loadConfig() {
+        $.get(join(API_ENDPOINT, 'config'))
+            .done((config) => {
+                let attributes: string[] = [];
+                let main_attributes: [number, string][] = [];
+                let primary_key = '';
+                for (let key in config) {
+                    attributes.push(key);
+                    if (config[key].main_attribute !== undefined) {
+                        // This key is marked as a main attribute, which means
+                        // we'll use it on the list display. It's value is the
+                        // place it's supposed to have in the display, lower
+                        // first, higher last.
+                        main_attributes.push([config[key].main_attribute, key])
+                    }
+                    if (config[key].primary_key !== undefined && config[key].primary_key === true) {
+                        // This is key is marked as the primary key, which means
+                        // we can use it to uniquely identify a contact and use
+                        // to perfom PUT and DELETE on the API.
+                        primary_key = key;
+                    }
+                }
+                this.setState({
+                    config: {
+                        // Sort the main attributes by their value, to respect
+                        // the order the user wanted, and then get only the key
+                        main_attributes: main_attributes.sort((a, b) => a[0] - b[0]).map((item) => item[1]),
+                        attributes: attributes,
+                        primary_key: primary_key,
+                        raw_config: config,
+                    },
+                    isConfigLoaded: true
+                });
+            })
+            .fail((error) => {
+                this.setState({
+                    error: error,
+                    isConfigLoaded: false
+                });
+            })
+    }
+
+    loadContacts() {
         $.get(join(API_ENDPOINT, 'contact'))
             .done((contacts) => {
                 this.setState({
@@ -36,6 +92,11 @@ class Home extends Component<PropsType, StateType> {
                     isLoaded: false
                 });
             });
+    }
+
+    componentDidMount() {
+        this.loadConfig();
+        this.loadContacts();
     }
 
     render() {
