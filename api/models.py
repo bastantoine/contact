489e79213f449cbceacafb0c1216fada2da195f1
bastantoine <bastien.antoine69+github@gmail.com>
2021-04-16 18:43:58 +0200

    Delete all the files related to an instance on its deletion

--- api/models.py
+++ api/models.py
@@ -1,6 +1,19 @@
 from json import loads
+import os
 
+from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.orm import Mapper
+from sqlalchemy.engine import Connection
+
+from config import (
+    Config,
+    FILE_FIELDS,
+)
+
+# Load .env file in the env variables
+load_dotenv()
 
 db = SQLAlchemy()
 
@@ -13,3 +26,12 @@ class Contact(db.Model):
         infos = loads(self.infos)
         infos['id'] = self.id
         return infos
+
+@event.listens_for(Contact, 'after_delete')
+def clean_files_on_delete(_mapper: Mapper, _connection: Connection, target: Contact):
+    config = Config(os.environ.get('CONFIG_FILE', 'config.json'))
+    file_fields = [field.name for field in config.fields if field.field_type in FILE_FIELDS]
+    infos = loads(target.infos)
+    for field in file_fields:
+        if infos.get(field):
+            os.remove(os.path.join(os.environ.get('UPLOAD_FOLDER', 'uploads'), infos[field]))
