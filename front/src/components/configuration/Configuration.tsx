55f24677d946dc4c6990520e8ab43a17b8c6d376
bastantoine <bastien.antoine69+github@gmail.com>
2021-04-22 22:00:34 +0200

    Allow to reorder the fields in the config using drag-n-drop
    Code taken from https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd

--- front/src/components/configuration/Configuration.tsx
+++ front/src/components/configuration/Configuration.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Formik } from "formik";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 import { ConfigType, FieldConfigType } from "../Home";
 import ConfigurationField from "./ConfigurationField";
@@ -14,7 +15,8 @@ type PropsType = {
     },
 }
 type StateType = {
-    form_config: {[k: string]: {name: string, config: FieldConfigType}}
+    form_config: {[k: string]: {name: string, config: FieldConfigType}},
+    fields: string[],
 }
 
 class Configuration extends Component<PropsType, StateType> {
@@ -28,7 +30,10 @@ class Configuration extends Component<PropsType, StateType> {
         let form_config: {[k: string]: {name: string, config: FieldConfigType}} = {};
         for (let [fieldName, fieldConfig] of Object.entries(this.props.config.raw_config))
             form_config[fieldName] = {name: fieldName, config: fieldConfig};
-        this.state = {form_config: form_config};
+        this.state = {
+            form_config: form_config,
+            fields: Object.keys(form_config),
+        };
     }
 
     render() {
@@ -45,20 +50,59 @@ class Configuration extends Component<PropsType, StateType> {
                 // valide the inputs and won't display any error message that
                 // would mess up with the validation we already have.
                 <Form onSubmit={handleSubmit} noValidate>
-                    {Object.entries(this.state.form_config).map(([fieldKey, {name, config}]) => {
-                        return <ConfigurationField
-                            key={fieldKey}
-                            fieldName={name}
-                            fieldConfig={config}
-                        ></ConfigurationField>
-                    })}
+                    <DragDropContext onDragEnd={(result) => {
+                        // Don't do anything if the field has been dropped
+                        // outside of the droppable zone
+                        if (!result.destination) return;
+                        let from = result.source.index;
+                        let to = result.destination?.index;
+                        function array_move(arr: any[], from: number, to: number) {
+                            // Helper to move an element of an array from 'from' to 'to'
+                            // From https://stackoverflow.com/a/5306832
+                            if (to >= arr.length) {
+                                var k = to - arr.length + 1;
+                                while (k--) arr.push(undefined);
+                            }
+                            arr.splice(to, 0, arr.splice(from, 1)[0]);
+                            return arr;
+                        };
+                        this.setState({fields: array_move(this.state.fields, from, to)})
+                    }}>
+                        <Droppable droppableId="config-form-fields">
+                            {(provided) => (
+                                <div {...provided.droppableProps} ref={provided.innerRef}>
+                                    {this.state.fields.map((fieldKey, index) => {
+                                        const {name, config} = this.state.form_config[fieldKey];
+                                        return <Draggable key={fieldKey} draggableId={fieldKey} index={index}>
+                                            {(provided) => (
+                                                <div
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    ref={provided.innerRef}
+                                                >
+                                                    <ConfigurationField
+                                                        fieldName={name}
+                                                        fieldConfig={config}
+                                                    ></ConfigurationField>
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    })}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                 </Form>
             )}
             </Formik>
             <Button onClick={() => {
                 let form_config = this.state.form_config;
-                form_config[Math.random().toString(36).substring(7)] = {name: '', config: {type: ''}};
-                this.setState({form_config: form_config});
+                let fields = this.state.fields;
+                let new_field_key = Math.random().toString(36).substring(7);
+                fields.push(new_field_key);
+                form_config[new_field_key] = {name: '', config: {type: ''}};
+                this.setState({form_config: form_config, fields: fields});
             }}>Add new field</Button>
         </>
     }
