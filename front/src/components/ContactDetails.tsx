7261dde23680d1bd37f491fda851201a34356c6c
bastantoine <bastien.antoine69+github@gmail.com>
2021-06-03 20:09:40 +0200

    npx prettier --write src/components

--- front/src/components/ContactDetails.tsx
+++ front/src/components/ContactDetails.tsx
@@ -1,72 +1,129 @@
-import React, { useState } from "react";
-import { Button, ButtonGroup, Tab } from "react-bootstrap";
+import React, {useState} from "react";
+import {Button, ButtonGroup, Tab} from "react-bootstrap";
 
-import { ATTRIBUTE_TYPE_COMPONENT_MAPPING, TextComponent } from "./TypeComponents";
+import {
+    ATTRIBUTE_TYPE_COMPONENT_MAPPING,
+    TextComponent,
+} from "./TypeComponents";
 import ContactForm from "./contactForm/ContactForm";
-import { deepCopy, upperFirstLetter } from "../utils";
-import { ConfigType } from "./Home";
+import {deepCopy, upperFirstLetter} from "../utils";
+import {ConfigType} from "./Home";
 
 type PropsType = {
-    contact: any,
+    contact: any;
     config: {
-        main_attributes: string[],
-        attributes: string[],
-        primary_key: string,
-        raw_config: ConfigType,
-    },
-    fileInputChangeHandler: (attribute: string, file: File) => void,
-    editContactHandler: (id: string|number, values: {}) => Promise<void>,
-    deleteContactHandler: (id: string|number) => void
-}
+        main_attributes: string[];
+        attributes: string[];
+        primary_key: string;
+        raw_config: ConfigType;
+    };
+    fileInputChangeHandler: (attribute: string, file: File) => void;
+    editContactHandler: (id: string | number, values: {}) => Promise<void>;
+    deleteContactHandler: (id: string | number) => void;
+};
 
 // We have to add the property children, otherwise we have an error "Property
 // 'children' does not exist on type 'IntrinsicAttributes & PropsType'"
-function ContactDetails(props: PropsType & { children?: React.ReactNode}) {
+function ContactDetails(props: PropsType & {children?: React.ReactNode}) {
     const [isFormDisplayed, setIsFormDisplayed] = useState(false);
 
-    return <Tab.Pane eventKey={`#display-infos-contact-${props.contact[props.config.primary_key]}`}>
-        <dl>
-            {props.config.attributes.map((attribute) => {
-                // Display only if the attribute is not the primary key
-                if (attribute !== props.config.primary_key) {
-                    let has_display_name = props.config.raw_config[attribute].display_name
-                    let attribute_type = props.config.raw_config[attribute].type;
-                    let ComponentToUse = ATTRIBUTE_TYPE_COMPONENT_MAPPING[attribute_type] || TextComponent;
-                    return <React.Fragment key={`infos-contact-${props.contact[props.config.primary_key]}-${attribute}`}>
-                        <dt>{has_display_name ? props.config.raw_config[attribute].display_name : upperFirstLetter(attribute)}</dt>
-                        <dd><ComponentToUse value={props.contact[attribute] ? props.contact[attribute] : ''} extra_params={props.config.raw_config[attribute].additional_type_parameters}></ComponentToUse></dd>
-                    </React.Fragment>
-                }
-                return <React.Fragment key={`infos-contact-${props.contact[props.config.primary_key]}-${attribute}`}></React.Fragment>
-            })}
-        </dl>
-        <ButtonGroup>
-            <Button variant="primary" size="sm" onClick={() => setIsFormDisplayed(!isFormDisplayed)}>
-                Edit
-            </Button>
-            <Button variant="danger" size="sm" onClick={() => props.deleteContactHandler(props.contact[props.config.primary_key])}>
-                Delete
-            </Button>
-        </ButtonGroup>
-        {/* Pass a deep copy of the instance of contact. This way, every
+    return (
+        <Tab.Pane
+            eventKey={`#display-infos-contact-${
+                props.contact[props.config.primary_key]
+            }`}
+        >
+            <dl>
+                {props.config.attributes.map((attribute) => {
+                    // Display only if the attribute is not the primary key
+                    if (attribute !== props.config.primary_key) {
+                        let has_display_name =
+                            props.config.raw_config[attribute].display_name;
+                        let attribute_type =
+                            props.config.raw_config[attribute].type;
+                        let ComponentToUse =
+                            ATTRIBUTE_TYPE_COMPONENT_MAPPING[attribute_type] ||
+                            TextComponent;
+                        return (
+                            <React.Fragment
+                                key={`infos-contact-${
+                                    props.contact[props.config.primary_key]
+                                }-${attribute}`}
+                            >
+                                <dt>
+                                    {has_display_name
+                                        ? props.config.raw_config[attribute]
+                                              .display_name
+                                        : upperFirstLetter(attribute)}
+                                </dt>
+                                <dd>
+                                    <ComponentToUse
+                                        value={
+                                            props.contact[attribute]
+                                                ? props.contact[attribute]
+                                                : ""
+                                        }
+                                        extra_params={
+                                            props.config.raw_config[attribute]
+                                                .additional_type_parameters
+                                        }
+                                    ></ComponentToUse>
+                                </dd>
+                            </React.Fragment>
+                        );
+                    }
+                    return (
+                        <React.Fragment
+                            key={`infos-contact-${
+                                props.contact[props.config.primary_key]
+                            }-${attribute}`}
+                        ></React.Fragment>
+                    );
+                })}
+            </dl>
+            <ButtonGroup>
+                <Button
+                    variant="primary"
+                    size="sm"
+                    onClick={() => setIsFormDisplayed(!isFormDisplayed)}
+                >
+                    Edit
+                </Button>
+                <Button
+                    variant="danger"
+                    size="sm"
+                    onClick={() =>
+                        props.deleteContactHandler(
+                            props.contact[props.config.primary_key]
+                        )
+                    }
+                >
+                    Delete
+                </Button>
+            </ButtonGroup>
+            {/* Pass a deep copy of the instance of contact. This way, every
             change made to the values won't be reflect in the original object.
             Usefull since we transform each lists by joining their values with a
             comma to show them correctly in the input, but still need the lists
             almost everywhere else. */}
-        {isFormDisplayed ? <ContactForm
-            initial_value={deepCopy(props.contact)}
-            config={props.config}
-            fileInputChangeHandler={props.fileInputChangeHandler}
-            submitHandler={((values: {}) => {
-                return props.editContactHandler(
-                    props.contact[props.config.primary_key],
-                    values
-                )
-            })}
-            submitButtonMessage={"Edit the contact"}
-            >
-        </ContactForm> : <></>}
-    </Tab.Pane>
+            {isFormDisplayed ? (
+                <ContactForm
+                    initial_value={deepCopy(props.contact)}
+                    config={props.config}
+                    fileInputChangeHandler={props.fileInputChangeHandler}
+                    submitHandler={(values: {}) => {
+                        return props.editContactHandler(
+                            props.contact[props.config.primary_key],
+                            values
+                        );
+                    }}
+                    submitButtonMessage={"Edit the contact"}
+                ></ContactForm>
+            ) : (
+                <></>
+            )}
+        </Tab.Pane>
+    );
 }
 
-export default ContactDetails
+export default ContactDetails;
