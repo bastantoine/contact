f8810e09a4171da4d660339256c3677b3d5abe13
bastantoine <bastien.antoine69+github@gmail.com>
2021-06-10 18:19:03 +0200

    We cannot have duplicate keys because a python dict cannot have more than one key with the same name

--- api/config.py
+++ api/config.py
@@ -234,12 +234,6 @@ def validate_config(config: dict):
     MISSING_PARAMETER_FROM_FIELD_TEMPLATE = 'Missing parameter "{param}" from field "{field}"'
     INVALID_VALUE_OF_PARAMETER = 'Invalid value of parameter "{param}" for field "{field}": {value}'
 
-    # Config mustn't have two identical keys
-    keys = list(config.keys())
-    for key in keys:
-        if keys.count(key) > 1:
-            raise InvalidConfigException(key, '', f'Duplicate key found: {key}')
-
     # Config must have one and only one parameter marked as primary_key
     primary_key = {k: v for k, v in config.items() if v.get('primary_key')}
     if len(primary_key) == 0:
